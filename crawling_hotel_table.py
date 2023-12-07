e7aa36db1fa7142e622b1694d8a93f2cdf8551aa
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-08 07:28:41 +0900

    solve error using try catch error with mainreive:chat_gpt subtract

--- crawling_hotel_table.py
+++ crawling_hotel_table.py
@@ -38,40 +38,53 @@ service = Service(ChromeDriverManager().install())
 # WebDriver 인스턴스 생성
 driver = webdriver.Chrome(service=service, options=options)
 
+
+def extract_element_text(driver, css_selector):
+    """요소의 텍스트를 추출하거나 빈 문자열을 반환합니다."""
+    try:
+        return driver.find_element(By.CSS_SELECTOR, css_selector).text
+    except:
+        return ""
+
+
 for hotel_id in hotel_ids:
     hotel_id = hotel_id[0]  # 첫 번째 열이 HOTEL_ID
     url = f"https://www.yanolja.com/hotel/{hotel_id}"
 
     driver.get(url)
-    time.sleep(5)  # 웹페이지 로드 대기
+    time.sleep(1)  # 웹페이지 로드 대기
 
-    # 호텔 정보 추출
-    hotel_name = driver.find_element(
-        By.CSS_SELECTOR,
+    # 호텔 정보 추출 (예외 처리 추가)
+    hotel_name = extract_element_text(
+        driver,
         "#__next > div > div > main > article > div.css-1cc3d9 > div.css-6pnu6y > div.css-11vo59c > div.css-1g3ik0v",
-    ).text
-    hotel_location = driver.find_element(
-        By.CSS_SELECTOR,
+    )
+    hotel_location = extract_element_text(
+        driver,
         "#__next > div > div > main > article > div.css-c45a2y > div > section > div:nth-child(2) > section > div > div > div.css-cxbger > div.address.css-18ufud2 > span",
-    ).text
-    hotel_rating = driver.find_element(
-        By.CSS_SELECTOR,
+    )
+    hotel_rating = extract_element_text(
+        driver,
         "#__next > div > div > main > article > div.css-1cc3d9 > div.css-6pnu6y > div.css-14lo8yk > div.css-ubovjv > span.css-1decpwd",
-    ).text
-    hotel_mainreview = driver.find_element(
-        By.CSS_SELECTOR,
-        "#__next > div > div > main > article > div.css-1cc3d9 > div.css-6pnu6y > div.css-bpv7fg > div.css-x90eyc > div > div.css-1eclj0b > div:nth-child(1) > div > div > div",
-    ).text
+    )
+    hotel_mainreview_xpath = "/html/body/div/div/div/main/article/div[1]/div[1]/div[5]/div[3]/div/div/div[1]/div/div/div"
+    try:
+        hotel_mainreview = driver.find_element(By.XPATH, hotel_mainreview_xpath).text
+    except:
+        hotel_mainreview = ""  # XPath를 찾을 수 없는 경우 빈 문자열 사용
 
-    # 호텔 추가 정보 추출
-    driver.find_element(
-        By.CSS_SELECTOR,
-        "#__next > div > div > main > article > div.css-c45a2y > div > section > div:nth-child(5) > div > div > div.css-14fkj0f > div > button",
-    ).click()
-    time.sleep(3)  # 추가 정보 로드 대기
-    hotel_infomation = driver.find_element(
-        By.CSS_SELECTOR, "#BOTTOM_SHEET > div.css-gqqlqe > div.css-1ulzvpi > div"
-    ).text
+    # 호텔 추가 정보 추출 (예외 처리 추가)
+    try:
+        driver.find_element(
+            By.CSS_SELECTOR,
+            "#__next > div > div > main > article > div.css-c45a2y > div > section > div:nth-child(5) > div > div > div.css-14fkj0f > div > button",
+        ).click()
+        time.sleep(1)  # 추가 정보 로드 대기
+        hotel_infomation = extract_element_text(
+            driver, "#BOTTOM_SHEET > div.css-gqqlqe > div.css-1ulzvpi > div"
+        )
+    except:
+        hotel_infomation = ""
 
     # 데이터베이스에 저장
     sql = """
