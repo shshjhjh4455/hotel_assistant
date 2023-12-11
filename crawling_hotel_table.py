130506b367e4323d0bd24164a464f126e2cdc171
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-12 04:40:26 +0900

    호텔 테이블 크롤링 프로세스 개선

--- crawling_hotel_table.py
+++ crawling_hotel_table.py
@@ -1,119 +1,125 @@
+import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pyodbc
-
-# 데이터베이스 연결 설정
-server = "127.0.0.1"
-username = "sa"
-password = "Hotelchat44"
-# database = "your_database_name"  # 본인의 데이터베이스 이름으로 변경
-
-cnxn = pyodbc.connect(
-    "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-    + server
-    + ";DATABASE="
-    # + database
-    + ";UID="
-    + username
-    + ";PWD="
-    + password
-)
-cursor = cnxn.cursor()
-
-# HOTEL_ID 조회
-cursor.execute("SELECT HOTEL_ID FROM HOTEL")
-hotel_ids = cursor.fetchall()
-
-# Chrome 옵션 설정
-options = Options()
-options.add_argument("--incognito")
-
-# WebDriver 서비스
-service = Service(ChromeDriverManager().install())
-
-# WebDriver 인스턴스 생성
-driver = webdriver.Chrome(service=service, options=options)
+import re
 
 
-def extract_element_text(driver, css_selector):
-    """요소의 텍스트를 추출하거나 빈 문자열을 반환합니다."""
+def process_hotel_page(hotel_id):
+    options = Options()
+    options.add_argument("--incognito")
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+
     try:
-        return driver.find_element(By.CSS_SELECTOR, css_selector).text
-    except:
-        return ""
+        url = f"https://www.yanolja.com/hotel/{hotel_id}"
+        driver.get(url)
+        time.sleep(1)
 
-
-for hotel_id in hotel_ids:
-    hotel_id = hotel_id[0]  # 첫 번째 열이 HOTEL_ID
-    url = f"https://www.yanolja.com/hotel/{hotel_id}"
-
-    driver.get(url)
-    time.sleep(1)  # 웹페이지 로드 대기
-
-    # 호텔 정보 추출 (예외 처리 추가)
-    hotel_name = extract_element_text(
-        driver,
-        "#__next > div > div > main > article > div.css-1cc3d9 > div.css-6pnu6y > div.css-11vo59c > div.css-1g3ik0v",
-    )
-    hotel_location = extract_element_text(
-        driver,
-        "#__next > div > div > main > article > div.css-c45a2y > div > section > div:nth-child(2) > section > div > div > div.css-cxbger > div.address.css-18ufud2 > span",
-    )
-    hotel_rating = extract_element_text(
-        driver,
-        "#__next > div > div > main > article > div.css-1cc3d9 > div.css-6pnu6y > div.css-14lo8yk > div.css-ubovjv > span.css-1decpwd",
-    )
-    hotel_mainreview_xpath = "/html/body/div/div/div/main/article/div[1]/div[1]/div[5]/div[3]/div/div/div[1]/div/div/div"
-    try:
-        hotel_mainreview = driver.find_element(By.XPATH, hotel_mainreview_xpath).text
-    except:
-        hotel_mainreview = ""  # XPath를 찾을 수 없는 경우 빈 문자열 사용
-
-    # 호텔 추가 정보 추출 (예외 처리 추가)
-    try:
-        driver.find_element(
+        hotel_name = driver.find_element(
             By.CSS_SELECTOR,
-            "#__next > div > div > main > article > div.css-c45a2y > div > section > div:nth-child(5) > div > div > div.css-14fkj0f > div > button",
-        ).click()
-        time.sleep(1)  # 추가 정보 로드 대기
-        hotel_infomation = extract_element_text(
-            driver, "#BOTTOM_SHEET > div.css-gqqlqe > div.css-1ulzvpi > div"
-        )
-    except:
-        hotel_infomation = ""
+            "#__next > div > div > main > article > div.css-1cc3d9 > div.css-6pnu6y > div.css-11vo59c > div.css-1g3ik0v",
+        ).text
+        hotel_location = driver.find_element(
+            By.CSS_SELECTOR,
+            "#__next > div > div > main > article > div.css-c45a2y > div > section > div:nth-child(2) > section > div > div > div.css-cxbger > div.address.css-18ufud2 > span",
+        ).text
+        hotel_rating = driver.find_element(
+            By.CSS_SELECTOR,
+            "#__next > div > div > main > article > div.css-1cc3d9 > div.css-6pnu6y > div.css-14lo8yk > div.css-ubovjv > span.css-1decpwd",
+        ).text
 
-    # 데이터베이스에 저장
+        review_count_element = driver.find_element(
+            By.CSS_SELECTOR, "span.css-1sg2lsz"
+        ).text
+        review_count = int(re.sub(r"[^\d]", "", review_count_element))
+        print(f"Completed processing hotel ID: {hotel_id}")
+
+        return hotel_id, hotel_name, hotel_location, hotel_rating, review_count
+
+    except Exception as e:
+        print(f"Error processing hotel ID {hotel_id}: {e}")
+        return hotel_id, None, None, None, None
+    finally:
+        driver.quit()
+
+
+def update_database(hotel_data):
+    server = "127.0.0.1"
+    username = "sa"
+    password = "Hotelchat44"
+
+    cnxn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + server
+        + ";UID="
+        + username
+        + ";PWD="
+        + password
+    )
+    cursor = cnxn.cursor()
+
     sql = """
     MERGE INTO HOTEL AS target
-    USING (VALUES (?, ?, ?, ?, ?, ?)) AS source (HOTEL_ID, NAME, LOCATION, RATING, MAINREVIEW, INFOMATION)
+    USING (VALUES (?, ?, ?, ?, ?)) AS source (HOTEL_ID, NAME, LOCATION, RATING, REVIEW_COUNT)
     ON target.HOTEL_ID = source.HOTEL_ID
     WHEN MATCHED THEN
         UPDATE SET
             NAME = source.NAME,
             LOCATION = source.LOCATION,
             RATING = source.RATING,
-            MAINREVIEW = source.MAINREVIEW,
-            INFOMATION = source.INFOMATION
+            REVIEW_COUNT = source.REVIEW_COUNT
     WHEN NOT MATCHED THEN
-        INSERT (HOTEL_ID, NAME, LOCATION, RATING, MAINREVIEW, INFOMATION)
-        VALUES (source.HOTEL_ID, source.NAME, source.LOCATION, source.RATING, source.MAINREVIEW, source.INFOMATION);
+        INSERT (HOTEL_ID, NAME, LOCATION, RATING, REVIEW_COUNT)
+        VALUES (source.HOTEL_ID, source.NAME, source.LOCATION, source.RATING, source.REVIEW_COUNT);
     """
-    cursor.execute(
-        sql,
-        hotel_id,
-        hotel_name,
-        hotel_location,
-        hotel_rating,
-        hotel_mainreview,
-        hotel_infomation,
-    )
-    cnxn.commit()
 
-# 드라이버 및 데이터베이스 연결 종료
-driver.quit()
-cursor.close()
-cnxn.close()
+    for hotel_id, hotel_name, hotel_location, hotel_rating, review_count in hotel_data:
+        if hotel_name:  # Only update if data is valid
+            cursor.execute(
+                sql, hotel_id, hotel_name, hotel_location, hotel_rating, review_count
+            )
+    print("Database update completed.")  # 데이터베이스 업데이트 완료
+
+    cnxn.commit()
+    cursor.close()
+    cnxn.close()
+
+
+def main():
+    server = "127.0.0.1"
+    username = "sa"
+    password = "Hotelchat44"
+    cnxn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + server
+        + ";UID="
+        + username
+        + ";PWD="
+        + password
+    )
+    cursor = cnxn.cursor()
+
+    cursor.execute("SELECT HOTEL_ID FROM HOTEL")
+    hotel_ids = [row[0] for row in cursor.fetchall()]
+    cursor.close()
+    cnxn.close()
+
+    hotel_data = []
+    print("Starting hotel data processing...")  # 크롤링 시작
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_hotel = {
+            executor.submit(process_hotel_page, hotel_id): hotel_id
+            for hotel_id in hotel_ids
+        }
+        for future in concurrent.futures.as_completed(future_to_hotel):
+            result = future.result()
+            hotel_data.append(result)
+    print("All hotel data processing completed.")  # 크롤링 완료
+    update_database(hotel_data)
+
+
+if __name__ == "__main__":
+    main()
