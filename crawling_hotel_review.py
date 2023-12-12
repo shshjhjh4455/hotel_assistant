279e0b01b3f7819622760415eedb6274f2aa8e11
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-12 13:34:34 +0900

    업데이트

--- crawling_hotel_review.py
+++ crawling_hotel_review.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import time
 import re
 import pyodbc
@@ -5,44 +6,48 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
-import concurrent.futures
 
 
 def process_review_page(driver, hotel_id):
-    print(f"Processing reviews for hotel ID: {hotel_id}") 
-    url = f"https://place-site.yanolja.com/places/{hotel_id}/review"
-    driver.get(url)
+    try:
+        print(f"Processing reviews for hotel ID: {hotel_id}")
+        url = f"https://place-site.yanolja.com/places/{hotel_id}/review"
+        driver.get(url)
 
-    # 스크롤 다운하여 모든 리뷰 로드
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)  # 페이지 로드 대기
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
+        # 스크롤 다운하여 모든 리뷰 로드
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(5)  # 페이지 로드 대기
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
 
-    reviews = []
-    index = 1
-    while True:
-        try:
-            rating_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-176xgwq > div:nth-child(1) > div > div.css-ledymh > div"
-            comment_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-1v46kci > div > div"
+        reviews = []
+        index = 1
+        print(f"Review found for index {index}")
+        while True:
+            try:
+                rating_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-176xgwq > div:nth-child(1) > div > div.css-ledymh > div"
+                comment_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-1v46kci > div > div"
 
-            rating_elements = driver.find_elements(By.CSS_SELECTOR, rating_selector)
-            rating = calculate_rating(rating_elements)
+                rating_elements = driver.find_elements(By.CSS_SELECTOR, rating_selector)
+                rating = calculate_rating(rating_elements)
 
-            comment_element = driver.find_element(By.CSS_SELECTOR, comment_selector)
-            comment = comment_element.text
+                comment_element = driver.find_element(By.CSS_SELECTOR, comment_selector)
+                comment = comment_element.text
 
-            reviews.append((hotel_id, rating, comment))
+                reviews.append((hotel_id, rating, comment))
 
-            index += 1
-        except NoSuchElementException:
-            break  # 모든 리뷰 처리 완료
+                index += 1
+            except NoSuchElementException:
+                print(f"No more reviews found at index {index}, moving to next hotel")
+                break  # 모든 리뷰 처리 완료
+        return reviews
+    except Exception as e:
+        print(f"Error processing reviews for hotel ID {hotel_id}: {e}")
     print(f"Completed reviews for hotel ID: {hotel_id}")
-    return reviews
 
 
 def calculate_rating(rating_elements):
@@ -80,7 +85,6 @@ def main():
     username = "sa"
     password = "Hotelchat44"
 
-    # 데이터베이스 연결 및 HOTEL_ID 조회
     cnxn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
         + server
@@ -97,14 +101,21 @@ def main():
 
     options = Options()
     options.add_argument("--incognito")
-    driver = webdriver.Chrome(options=options)
 
-    # 각 호텔 ID에 대한 리뷰 처리
-    for hotel_id in hotel_ids:
-        reviews = process_review_page(driver, hotel_id)
-        save_reviews_to_database(reviews, server, username, password)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = []
+        for hotel_id in hotel_ids:
+            driver = webdriver.Chrome(options=options)  # 각 스레드에 독립적인 드라이버 인스턴스 할당
+            future = executor.submit(process_review_page, driver, hotel_id)
+            futures.append(future)
 
-    driver.quit()
+        # 작업 결과를 수집하여 데이터베이스에 저장
+        for future in concurrent.futures.as_completed(futures):
+            reviews = future.result()
+            if reviews:
+                save_reviews_to_database(reviews, server, username, password)
+
+    print("All review processing tasks completed.")
 
 
 if __name__ == "__main__":
