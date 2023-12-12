f2b59d6fc4854611c90df1770153378406dab76f
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-12 13:22:39 +0900

    리뷰 크롤링

--- crawling_hotel_review.py
+++ crawling_hotel_review.py
@@ -1,60 +1,59 @@
-import concurrent.futures
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 import time
 import re
 import pyodbc
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+import concurrent.futures
 
 
-def process_review_page(hotel_id, server, username, password):
-    options = Options()
-    options.add_argument("--incognito")
-    driver = webdriver.Chrome(options=options)
+def process_review_page(driver, hotel_id):
+    print(f"Processing reviews for hotel ID: {hotel_id}") 
+    url = f"https://place-site.yanolja.com/places/{hotel_id}/review"
+    driver.get(url)
 
-    try:
-        url = f"https://place-site.yanolja.com/places/{hotel_id}/review"
-        driver.get(url)
+    # 스크롤 다운하여 모든 리뷰 로드
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)  # 페이지 로드 대기
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
-        # 페이지를 계속 아래로 스크롤하여 리뷰 로드
-        last_height = driver.execute_script("return document.body.scrollHeight")
-        review_count = 0
-        while review_count < 500:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(1)  # 페이지 로드 대기
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
+    reviews = []
+    index = 1
+    while True:
+        try:
+            rating_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-176xgwq > div:nth-child(1) > div > div.css-ledymh > div"
+            comment_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-1v46kci > div > div"
 
-            review_elements = driver.find_elements(
-                By.CSS_SELECTOR, "div.css-1v46kci > div > div"
-            )
-            review_count = len(review_elements)
+            rating_elements = driver.find_elements(By.CSS_SELECTOR, rating_selector)
+            rating = calculate_rating(rating_elements)
 
-        # 리뷰 정보 추출 및 저장
-        save_review_data(driver, hotel_id, server, username, password)
+            comment_element = driver.find_element(By.CSS_SELECTOR, comment_selector)
+            comment = comment_element.text
 
-    except Exception as e:
-        print(f"Error processing review page for hotel ID {hotel_id}: {e}")
-    finally:
-        driver.quit()
+            reviews.append((hotel_id, rating, comment))
 
-
-def calculate_rating(rating_element):
-    # 모든 별(SVG) 요소를 찾음
-    stars = rating_element.find_elements(By.CSS_SELECTOR, "svg.css-189aa3t")
-    filled_stars_count = 0
-
-    # 색이 채워진 별의 개수를 세어 평점 계산
-    for star in stars:
-        if 'fill="#fdbd00"' in star.get_attribute("outerHTML"):
-            filled_stars_count += 1
-
-    return filled_stars_count
+            index += 1
+        except NoSuchElementException:
+            break  # 모든 리뷰 처리 완료
+    print(f"Completed reviews for hotel ID: {hotel_id}")
+    return reviews
 
 
-def save_review_data(driver, hotel_id, server, username, password):
+def calculate_rating(rating_elements):
+    star_count = 0
+    for star in rating_elements:
+        if "fdbd00" in star.get_attribute("innerHTML"):
+            star_count += 1
+    return star_count
+
+
+def save_reviews_to_database(reviews, server, username, password):
     cnxn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
         + server
@@ -65,53 +64,15 @@ def save_review_data(driver, hotel_id, server, username, password):
     )
     cursor = cnxn.cursor()
 
-    # 모든 리뷰 요소를 찾음
-    review_blocks = driver.find_elements(
-        By.CSS_SELECTOR,
-        "#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div",
-    )
-    review_count = 0
-
-    for review_block in review_blocks:
-        if review_count >= 500:  # 최대 500개의 리뷰 처리
-            break
-
-        # 별점 추출
-        rating_element = review_block.find_element(
-            By.CSS_SELECTOR,
-            f"__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child(1~여러개) > div > div > div.css-176xgwq > div:nth-child(1) > div > div.css-ledymh > div" ,
-        )
-        rating = calculate_rating(rating_element)
-
-        # 코멘트 추출
-        comment_element = review_block.find_element(
-            By.CSS_SELECTOR, "div > div > div.css-1v46kci > div > div"
-        )
-        comment = comment_element.text
-
-        # 데이터베이스에 저장
-        cursor.execute(
-            "INSERT INTO REVIEW (HOTEL_ID, RATING, COMMENT) VALUES (?, ?, ?)",
-            hotel_id,
-            rating,
-            comment,
-        )
-        review_count += 1
-
+    sql = """
+    INSERT INTO REVIEW (HOTEL_ID, RATING, COMMENT)
+    VALUES (?, ?, ?);
+    """
+    for review in reviews:
+        cursor.execute(sql, review)
     cnxn.commit()
     cursor.close()
     cnxn.close()
-
-
-def calculate_rating(rating_element):
-    stars = rating_element.find_elements(By.CSS_SELECTOR, "svg.css-189aa3t")
-    filled_stars_count = 0
-
-    for star in stars:
-        if 'fill="#fdbd00"' in star.get_attribute("outerHTML"):
-            filled_stars_count += 1
-
-    return filled_stars_count
 
 
 def main():
@@ -119,6 +80,7 @@ def main():
     username = "sa"
     password = "Hotelchat44"
 
+    # 데이터베이스 연결 및 HOTEL_ID 조회
     cnxn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
         + server
@@ -128,18 +90,21 @@ def main():
         + password
     )
     cursor = cnxn.cursor()
-
     cursor.execute("SELECT HOTEL_ID FROM HOTEL")
     hotel_ids = [row[0] for row in cursor.fetchall()]
     cursor.close()
     cnxn.close()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        futures = [
-            executor.submit(process_review_page, hotel_id, server, username, password)
-            for hotel_id in hotel_ids
-        ]
-        concurrent.futures.wait(futures)
+    options = Options()
+    options.add_argument("--incognito")
+    driver = webdriver.Chrome(options=options)
+
+    # 각 호텔 ID에 대한 리뷰 처리
+    for hotel_id in hotel_ids:
+        reviews = process_review_page(driver, hotel_id)
+        save_reviews_to_database(reviews, server, username, password)
+
+    driver.quit()
 
 
 if __name__ == "__main__":
