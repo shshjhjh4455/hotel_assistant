19eb7757efe61b8acbf4dc9d07cd0a5ad206145b
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-12 16:38:24 +0900

    리뷰 크롤링 정상작동

--- crawling_hotel_review.py
+++ crawling_hotel_review.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 def process_review_page(driver, hotel_id):
@@ -16,7 +17,6 @@ def process_review_page(driver, hotel_id):
         url = f"https://place-site.yanolja.com/places/{hotel_id}/review"
         driver.get(url)
         time.sleep(3)
-
         # 스크롤 다운 로직
         last_height = driver.execute_script("return document.body.scrollHeight")
         while True:
@@ -32,45 +32,75 @@ def process_review_page(driver, hotel_id):
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 new_height = driver.execute_script("return document.body.scrollHeight")
                 if new_height == last_height:
+                    driver.execute_script(
+                        "window.scrollTo(0, document.body.scrollHeight);"
+                    )
                     break
             last_height = new_height
 
         reviews = []
-        index = 1
+
+        # 페이지의 모든 섹션을 반복
+        section_index = 1
         while True:
             try:
-                rating_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-176xgwq > div:nth-child(1) > div > div.css-ledymh > div"
-                comment_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child(1) > div:nth-child({index}) > div > div > div.css-1v46kci > div > div"
+                # 섹션 내의 리뷰 아이템 반복
+                for item_index in range(1, 21):
+                    comment_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child({section_index}) > div:nth-child({item_index}) > div > div > div.css-1v46kci"
+                    rating_selector = f"#__next > div > div > main > div > div:nth-child(4) > div:nth-child({section_index}) > div:nth-child({item_index}) > div > div > div.css-176xgwq > div:nth-child(1) > div > div.css-ledymh > div"
 
-                rating_elements = driver.find_elements(By.CSS_SELECTOR, rating_selector)
-                rating = calculate_rating(rating_elements)
+                    WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located(
+                            (By.CSS_SELECTOR, comment_selector)
+                        )
+                    )
+                    WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located(
+                            (By.CSS_SELECTOR, rating_selector)
+                        )
+                    )
 
-                comment_element = driver.find_element(By.CSS_SELECTOR, comment_selector)
-                comment = comment_element.text
+                    comment_element = driver.find_element(
+                        By.CSS_SELECTOR, comment_selector
+                    )
+                    rating_element = driver.find_element(
+                        By.CSS_SELECTOR, rating_selector
+                    )
 
-                reviews.append((hotel_id, rating, comment))
-                # print(reviews)
+                    comment = comment_element.text
+                    rating = calculate_rating(rating_element)
 
-                print(f"Review found for index {index}")
+                    reviews.append((hotel_id, rating, comment))
+                    print(f"Review {item_index} in section {section_index} added")
 
-                index += 1
-            except NoSuchElementException:
-                print(f"No more reviews found at index {index}, moving to next hotel")
-                break  # 모든 리뷰 처리 완료
-        return reviews
+                section_index += 1
+
+            except TimeoutException:
+                print(
+                    f"No more reviews to load in section {section_index}, moving to next section"
+                )
+                break
+
     except Exception as e:
         print(f"Error processing reviews for hotel ID {hotel_id}: {e}")
     finally:
         driver.quit()
         print(f"Completed reviews for hotel ID: {hotel_id}")
 
+    return reviews
 
-def calculate_rating(rating_elements):
-    star_count = 0
-    for star in rating_elements:
-        if "fdbd00" in star.get_attribute("innerHTML"):
-            star_count += 1
-    return star_count
+
+def calculate_rating(rating_element):
+    # 별 모양의 HTML을 가져옴
+    star_html = rating_element.get_attribute("outerHTML")
+
+    # 각 <path> 태그 내의 'fill="#fdbd00"' 빈도 계산
+    filled_stars_count = 0
+    for path_tag in star_html.split("<path"):
+        if 'fill="#fdbd00"' in path_tag:
+            filled_stars_count += 1
+
+    return filled_stars_count
 
 
 def save_reviews_to_database(reviews, server, username, password):
@@ -127,10 +157,6 @@ def main():
             save_reviews_to_database(reviews, server, username, password)
 
     print("All review processing tasks completed.")
-
-
-if __name__ == "__main__":
-    main()
 
 
 if __name__ == "__main__":
