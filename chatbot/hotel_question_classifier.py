56877c85ae19da7cd36bdff9300a06fd9e75083e
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-12-14 04:51:24 +0900

    성능 형편없음

--- chatbot/hotel_question_classifier.py
+++ chatbot/hotel_question_classifier.py
@@ -1,27 +1,26 @@
 from transformers import BertModel, BertTokenizer
 import torch
-
+import torch.nn as nn
 
 class HotelQuestionClassifier:
     def __init__(self):
-        self.model_name = "monologg/kobert"
+        self.model_name = 'monologg/kobert'
         self.tokenizer = BertTokenizer.from_pretrained(self.model_name)
         self.model = BertModel.from_pretrained(self.model_name)
+        # 선형 분류 레이어 추가
+        self.classifier = nn.Linear(768, 1)
 
     def classify(self, question):
-        inputs = self.tokenizer(
-            question, return_tensors="pt", max_length=512, truncation=True
-        )
+        inputs = self.tokenizer(question, return_tensors='pt', max_length=512, truncation=True)
         with torch.no_grad():
             outputs = self.model(**inputs)
-        # 여기에서 출력을 분석하여 질문이 호텔 관련 여부를 판단
-        # 예시 코드는 단순히 출력의 첫 번째 요소를 사용
-        # 실제 구현에서는 학습된 분류기 레이어 또는 로직이 필요
-        return outputs.last_hidden_state[0, 0, :].item() > 0  # 예시 임계값
-
+            avg_pool = torch.mean(outputs.last_hidden_state, dim=1)
+            logits = self.classifier(avg_pool)
+            probabilities = torch.sigmoid(logits)
+        return probabilities.item() > 0.1  # 임계값 설정
 
 # 사용 예
 classifier = HotelQuestionClassifier()
-question = "서울에 좋은 호텔 추천해줄래?"
+question = "배고파"
 is_hotel_related = classifier.classify(question)
 print(f"호텔 관련 질문: {is_hotel_related}")
